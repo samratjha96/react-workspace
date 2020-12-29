c0da0eb7cc39c2539025cdb3f07ad02baaeab986
Samrat Jha <samrat.jha@appian.com>
2020-12-28 19:14:19 -0500

    feat: add tailwind support

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,22 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-            test: /\.(js|jsx|mjs)$/,
-            exclude: /(node_modules)/,
-            loader: require.resolve('babel-loader'),
-            options: {
-              compact: true
-            }
-        },
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
+      {
+        test: /\.(js|jsx|mjs)$/,
+        exclude: /(node_modules)/,
+          loader: require.resolve('babel-loader'),
+          options: {
+            compact: true
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader",
+        ],
+      },
     ]
-}
+  }
 }
